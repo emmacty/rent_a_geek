a1a5f26e425c6795e0838fcd1746bf72edeafa16
Emma Cauty <emmacauty@gmail.com>
2023-08-21 16:20:56 +0200

    Added user foreign key to offers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_133819) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_141955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,9 +25,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_133819) do
     t.string "localisation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_134051) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -43,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_134051) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "offers", "users"
 end
