4c809483766ae348655e3d0949c14f374f952542
Emma Cauty <emmacauty@gmail.com>
2023-08-22 19:15:00 +0200

    Added some front for the main page and validation for the date

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,6 +5,7 @@ class Offer < ApplicationRecord
   validates :description, presence: true, length: { minimum: 20, maximum: 1000 }
   validates :user_id, presence: true
   validates :title, presence: true
+  validates :date, presence: true
   validates :localisation, presence: true
   validates :profession, presence: true
   validates :experience, presence: true
