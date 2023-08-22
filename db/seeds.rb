12af5b5eae46b2108276bd94cf5d106247724647
Emma Cauty <emmacauty@gmail.com>
2023-08-22 15:15:51 +0200

    Created index view for bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,72 +49,66 @@ user_6.save
 puts "Users created"
 puts "Creating offers..."
 Offer.create!(
-  user_id: User.first.id,
+  user_id: user_1.id,
   price: 780,
   description: 'Experienced Ruby on Rails developer available for freelance projects in Lyon, France.',
   title: 'Ruby on Rails Developer',
   date: Date.today,
-  status: 'Available',
   experience: 5,
   profession: 'Web Developer',
   localisation: 'Lyon, France'
 )
 
 Offer.create!(
-  user_id: User.second.id,
+  user_id: user_2.id,
   price: 600,
   description: 'Skilled front-end developer seeking remote work opportunities.',
   title: 'Front-End Developer',
   date: Date.today,
-  status: 'Available',
   experience: 3,
   profession: 'Web Developer',
   localisation: 'Marseille, France'
 )
 
 Offer.create!(
-  user_id: User.third.id,
+  user_id: user_3.id,
   price: 900,
   description: 'Backend developer with expertise in database design and optimization.',
   title: 'Backend Developer',
   date: Date.today,
-  status: 'Available',
   experience: 6,
   profession: 'Web Developer',
   localisation: 'Paris, France'
 )
 
 Offer.create!(
-  user_id: User.fourth.id,
+  user_id: user_4.id,
   price: 550,
   description: 'React.js expert available for contract work on web applications.',
   title: 'React.js Developer',
   date: Date.today,
-  status: 'Available',
   experience: 4,
   profession: 'Web Developer',
   localisation: 'Toulouse, France'
 )
 
 Offer.create!(
-  user_id: User.fifth.id,
+  user_id: user_5.id,
   price: 800,
   description: 'UI/UX designer passionate about creating user-friendly interfaces.',
   title: 'UI/UX Designer',
   date: Date.today,
-  status: 'Available',
   experience: 5,
   profession: 'Designer',
   localisation: 'Bordeaux, France'
 )
 
 Offer.create!(
-  user_id: User.last.id,
+  user_id: user_6.id,
   price: 700,
   description: 'DevOps engineer specializing in automation and CI/CD pipelines.',
   title: 'DevOps Engineer',
   date: Date.today,
-  status: 'Available',
   experience: 7,
   profession: 'DevOps',
   localisation: 'Nice, France'
