21d16f6b37160419a2e6871b9b191ede330594a5
Maiwenn BERTHO <maiwenn.berth@gmail.com>
2023-08-24 10:57:23 +0200

    testtt

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -11,4 +11,15 @@ class Offer < ApplicationRecord
   validates :experience, presence: true
   geocoded_by :localisation
   after_validation :geocode, if: :will_save_change_to_localisation?
+  include PgSearch::Model
+  multisearchable against: [:title, :description, :localisation, :profession]
+
+  pg_search_scope :global_search,
+  against: [ :title, :description, :localisation, :profession ],
+  associated_against: {
+    user: [ :first_name, :last_name ]
+  },
+  using: {
+    tsearch: { prefix: true }
+  }
 end
