a7809ef14ae553fb8fbf46367554991e5ab6b669
Emma Cauty <emmacauty@gmail.com>
2023-08-24 11:47:00 +0200

    Implemented date range in the offer form and changed offer description validation

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,7 +2,7 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :price, presence: true
-  validates :description, presence: true, length: { minimum: 20, maximum: 1000 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 1000 }
   validates :user_id, presence: true
   validates :title, presence: true
   validates :date, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_085805) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_085944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_085805) do
     t.integer "price"
     t.string "description"
     t.string "title"
-    t.date "date"
+    t.string "date"
     t.integer "experience"
     t.string "profession"
     t.string "localisation"
